1cc1f907eefe5a349bd6174a4223bd0bb8be142f
Rachael Kunz <rachaelmkunz@gmail.com>
2022-05-11 20:21:03 +0200

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import Header from "./components/Header";
-// import "./scss/abstracts/styles.scss";
 import "./scss/styles.scss";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header> */}
       <Header />
     </div>
   );
